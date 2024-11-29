90b6df21861fd35d154e07939144987fe7a86802
shoaibkhan73 <khan973973@gmail.com>
2024-11-30 00:08:11 +0500

    practice no 3 comleted

---
+++ script.js
@@ -0,0 +1,73 @@
+console.log("hellow i am shoaib");
+
+let age = 18 ;
+// let grace = 2 ;
+
+
+
+// console.log(age+grace);
+// console.log(age-grace);
+// console.log(age*grace);
+// console.log(age/grace);
+// console.log(age**grace);
+
+
+// assigment oprater
+// console.log("assigment latter")
+// console.log(age+=grace);
+// console.log(age-=grace);
+// console.log(age*=grace);
+// console.log(age/=grace);
+// console.log(age%=grace);
+// console.log(age**=grace);
+
+
+// comporation oprater
+// == equll to
+// !=  not equll to
+// === equal value and type
+// !==  not equal value and type
+// >  grater than
+// <  less than 
+// >=  grater than and equal to
+// <=  less than  and equal to
+// ? tenary oprater
+// you can check in terminal you wana to write frist Node then enter and then "3" === 3
+ 
+// if ( (age+grace)>18)
+if ( age!==18)
+{
+    console.log("your able for cnic");
+}
+
+else {
+    console.log("your not able for cnic");
+}
+
+
+// logical oprqater 
+// &&  logical and 
+// ||  logical or
+// !   logical not
+
+let a = 10;
+let b = 10;
+let c = 5;
+
+// javascrpit ternary oprater let d = c>=a ? (a-b) : (a-c)
+
+// let d = c>=a ? (a-b) : (a-c)
+// let d = c<=a ? (a-b) : (a-c)
+let d = c==a ? (a-b) : (a-c)
+ console.log(d);
+
+if (a!==b)
+    {
+        console.log("right");
+    }
+    else if ( a==b ){
+        console.log("warning");
+    }
+    else {
+        console.log("blast");
+    }
